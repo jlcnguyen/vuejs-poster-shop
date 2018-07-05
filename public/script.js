7c1875ac1bbb58269f7a9288aa3d7e40d275808f
Jessica Nguyen <jnguyen0@cs.washington.edu>
2018-07-05 11:18:08 -0700

    Add scroll loading and refine features
    - Add loading message to improve status visibility
    - Add default search results for landing page
    - Add price to each product displayed
    - Add transition to cart to improve aesthetics
    - Add scroll loading to avoid loading all results at once
    - Add "no more items" message for results list
    - Fix search results to display number of results instead
      number of results displayed

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 var PRICE = 9.99;
+var LOAD_NUM = 10
 
 new Vue({
   el: '#app',
@@ -6,16 +7,30 @@ new Vue({
     total: 0,
     items: [],
     cart: [],
-    newSearch: '',
-    lastSearch: ''
+    results: [],
+    newSearch: 'anime',
+    lastSearch: '',
+    loading: false,
+    price: PRICE
   },
   methods: {
+    appendItems: function() {
+      if (this.items.length < this.results.length) {
+        console.log("in here");
+        var append = this.results.slice(this.items.length, this.items.length + LOAD_NUM); // get next LOAD_NUM items
+        this.items = this.items.concat(append);
+      }
+    },
     onSubmit: function()  {
+      this.items = [];
+      this.loading = true;
       this.$http
         .get('/search/'.concat(this.newSearch))
         .then(function(res) {
-          this.items = res.data;
-          this.lastSearch = this.newSearch
+          this.lastSearch = this.newSearch;
+          this.results = res.data;
+          this.appendItems();
+          this.loading = false;
         })
       ;
 
@@ -23,23 +38,18 @@ new Vue({
     addItem: function(index) {
       this.total += PRICE;
       var item = this.items[index];
-      // var found = false;
       for (var i = 0; i < this.cart.length; i++) {
         if (this.cart[i].id === item.id) {
           this.cart[i].qty++;
-          // found = true;
-          // break;
           return;
         }
       }
-      // if (!found) {
-        this.cart.push({
-          id: item.id,
-          title: item.title,
-          qty: 1,
-          price: PRICE
-        });
-      // }
+      this.cart.push({
+        id: item.id,
+        title: item.title,
+        qty: 1,
+        price: PRICE
+      });
     },
     inc: function(item) { // getting a ref to item
       item.qty++;
@@ -52,7 +62,6 @@ new Vue({
         for (var i = 0; i < this.cart.length; i++) {
           if (this.cart[i].id === item.id) {
             this.cart.splice(i, 1);
-            // break;
             return;
           }
         }
@@ -63,5 +72,21 @@ new Vue({
     currency: function(price) {
       return '$'.concat(price.toFixed(2));
     }
+  },
+  computed: {
+    noMoreItems: function() {
+      return this.items.length === this.results.length && !this.loading;
+    }
+  },
+  mounted: function() {
+    this.onSubmit();
+    // Adding scroll scrollMonitor
+
+    var vueInstance = this;
+    var elem = document.getElementById('product-list-bottom');
+    var watcher = scrollMonitor.create(elem); // pass in DOM node reference
+    watcher.enterViewport(function() {
+      vueInstance.appendItems();
+    });
   }
 });
